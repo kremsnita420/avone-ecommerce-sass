cd823ae43e13ebcad232375c5c8962262707b7d6
kremsnita420 <duranovic.safet@gmail.com>
2022-02-14 15:52:57 +0100

    working new arrivals carousel

--- new-arrivals-carousel.js
+++ new-arrivals-carousel.js
@@ -1,12 +1,12 @@
 let slider = tns({
     container: '.slider__my-slider',
     'slideBy': '1',
-    items: 3,
     'speed': 400,
     'nav': false,
     autoplay: true,
     controls: true,
     autoplayButtonOutput: false,
-    gutter: 20,
+    items: 4,
+    gutter: 20
 
 })
