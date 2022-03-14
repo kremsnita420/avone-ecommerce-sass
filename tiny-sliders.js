0d673ce0d17bbce590ff2ca6fd60f9eca6f4b8a5
kremsnita420 <duranovic.safet@gmail.com>
2022-03-14 17:28:47 +0100

    blog section, info section, footer

--- tiny-sliders.js
+++ tiny-sliders.js
@@ -28,6 +28,41 @@ let slider = tns({
     }
 });
 
+
+let slider2 = tns({
+    container: '.slider__my-slider',
+    items: 1,
+    gutter: 20,
+    slideBy: '1',
+    autoplay: false,
+    "nav": false,
+    controlsContainer: ".slider__controls",
+    prevButton: ".slider__previous",
+    nextButton: ".slider__next",
+
+    responsive: {
+
+
+        1200: {
+            items: 4,
+            gutter: 20,
+        },
+        770: {
+            items: 3,
+            gutter: 20,
+        },
+        480: {
+            items: 2,
+
+        },
+
+
+
+
+    }
+});
+
+
 let slider3 = tns({
     container: '.blog__my-articles',
     items: 1,
