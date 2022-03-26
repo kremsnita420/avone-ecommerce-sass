3e316d5db3e69063c1e4cd9c0e6411acb26991b9
kremsnita420 <duranovic.safet@gmail.com>
2022-03-26 19:59:12 +0100

    collections grid and filters start

--- script.js
+++ script.js
@@ -87,4 +87,41 @@ const x = setInterval(function () {
     }
 }, 1000);
 
+// ! Timer 2
+const daysContainer1 = document.getElementById("days1")
+const hoursContainer1 = document.getElementById("hours1")
+const minutesContainer1 = document.getElementById("minutes1")
+const secondsContainer1 = document.getElementById("seconds1")
+// Set the date we're counting down to
+const countDownDate1 = new Date("dec 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+const x1 = setInterval(function () {
+
+    // Get today's date and time
+    const now1 = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    const distance1 = countDownDate1 - now1;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days1 = Math.floor(distance1 / (1000 * 60 * 60 * 24));
+    const hours1 = Math.floor((distance1 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes1 = Math.floor((distance1 % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds1 = Math.floor((distance1 % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="countdown-timer"
+    if (secondsContainer1 && distance1 > 0) {
+        daysContainer1.innerHTML = days1 < 10 ? `0${days1}` : `${days1}`
+        hoursContainer1.innerHTML = hours1 < 10 ? `0${hours1}` : `${hours1}`
+        minutesContainer1.innerHTML = minutes1 < 10 ? `0${minutes1}` : `${minutes1}`
+        secondsContainer1.innerHTML = seconds1 < 10 ? `0${seconds1}` : `${seconds1}`
+    }
+    // If the count down is finished, write some text
+    if (distance1 < 0) {
+        clearInterval(x1);
+        document.getElementsByClassName("countdown-timer1").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
 
