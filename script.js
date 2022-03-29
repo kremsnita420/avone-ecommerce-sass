e841d370f4bced61402663d1d3888fade4422480
kremsnita420 <duranovic.safet@gmail.com>
2022-03-29 11:49:18 +0200

    filters sidebar implementation

--- script.js
+++ script.js
@@ -36,6 +36,26 @@ cartOpen[0].addEventListener("click", () => {
 
 });
 
+// ! Sidebar filters toggle
+const filterOpen = document.getElementsByClassName('product-filter-btn')
+const filterClose = document.getElementsByClassName('product-filters-sidebar__close-icon')
+
+const sidebarFilters = document.getElementsByClassName('product-filters-sidebar')
+
+filterOpen[0].addEventListener("click", () => {
+    sidebarFilters[0].classList.add("product-filters-sidebar--visible")
+    sidebarFilters[0].classList.remove("product-filters-sidebar--hidden")
+    console.log('open')
+
+});
+
+filterClose[0].addEventListener("click", () => {
+    sidebarFilters[0].classList.add("product-filters-sidebar--hidden")
+    sidebarFilters[0].classList.remove("product-filters-sidebar--visible")
+    console.log('close')
+
+});
+
 
 // ! Accordion toggle
 const acc = document.getElementsByClassName("accordion-toggle");
