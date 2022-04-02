7b575810fedf15863e3764067f05a1f772cf67df
kremsnita420 <duranovic.safet@gmail.com>
2022-04-02 16:48:08 +0200

    Work on quick view modal

--- javascript/script.js
+++ javascript/script.js
@@ -92,7 +92,6 @@ const toggleModalNinth = () => {
 
 
 // ! Quick View Modal
-
 // modal open
 const modalOpen = document.getElementById('quick-view');
 // modal close
@@ -163,6 +162,26 @@ window.onclick = function (event) {
         //remove scrolling on body when modal is open
         bodyElement.classList.remove("stop-scroll");
 
+    }
+}
+
+//! Product QUantity selector
+
+function increaseCount(a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt(input.value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    input.value = value;
+}
+
+function decreaseCount(a, b) {
+    var input = b.nextElementSibling;
+    var value = parseInt(input.value, 10);
+    if (value > 1) {
+        value = isNaN(value) ? 0 : value;
+        value--;
+        input.value = value;
     }
 }
 
